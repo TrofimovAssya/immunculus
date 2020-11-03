dce33518fa44e379240c2cfabbf04e75eec486eb
TrofimovAssya <petitkalimero@gmail.com>
2020-11-03 10:58:59 -0500

    added config launch files

---
+++ relaunch_config3_main.sh
@@ -0,0 +1,7 @@
+gpu=$1
+prefix=$2
+ptname=$3
+savedir=$4
+python main.py --data-dir dataset --data-file ${ptname}_tcr_${prefix}.npy --target-file ${ptname}_targets.npy --dataset tcr --model tcr  --emb_size 10 --save-dir $savedir --epoch 500 --gpu-selection $gpu  --tcr-conv-layers-sizes 20 20 5 20 20 5 20 1 10 --mlp-layers-size 50 25 10 --load-folder $savedir 
+ 
+
